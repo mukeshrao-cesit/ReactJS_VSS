148f4043eec2cb830f5849d27ec20641b638a54b
Mukesh Rao Nagendrarao <mukeshrao.nagendrarao@cesltd.com>
2022-12-14 15:22:30 +0530

    class component with state

---
+++ Class-Component/src/App.jsx
@@ -0,0 +1,11 @@
+import ClassCompwithState from "./ClassCompwithState";
+
+function App() {
+  return (
+    <div>
+      <ClassCompwithState />
+    </div>
+  );
+}
+
+export default App;
---
+++ Class-Component/src/ClassCompwithState.jsx
@@ -0,0 +1,56 @@
+import { Component } from "react";
+
+class ClassCompwithState extends Component {
+  state = {
+    name: "",
+    password: "",
+    date: "",
+  };
+  handleInputChange = (e) => {
+    const { value, name } = e.target;
+    console.log(e.target);
+    if (!name) {
+      return;
+    }
+
+    this.setState({
+      [name]: value,
+    });
+  };
+  render() {
+    const { name, password, date } = this.state;
+    return (
+      <div>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={this.handleInputChange}
+        />
+        <label>Password </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={this.handleInputChange}
+        ></input>
+        <label>Date</label>
+        <input
+          type="date"
+          name="date"
+          value={date}
+          onChange={this.handleChange}
+        ></input>
+
+        <br></br>
+
+        <p>Name : {this.state.name}</p>
+        <p>Password : {this.state.password}</p>
+        <p>Date : {this.state.date}</p>
+      </div>
+    );
+  }
+}
+
+export default ClassCompwithState;
